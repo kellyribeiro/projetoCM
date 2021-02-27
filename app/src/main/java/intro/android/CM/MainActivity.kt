fa2091d369a86d163bdc7ae10bbdd7562f584680
Kelly Araujo <kellyaraujo1998@hotmail.com>
2021-02-27 19:28:11 +0000

    add nwe method A

---
+++ app/src/main/java/intro/android/CM/MainActivity.kt
@@ -0,0 +1,13 @@
+package intro.android.CM
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // adicionei o metodo A
+    }
+}
